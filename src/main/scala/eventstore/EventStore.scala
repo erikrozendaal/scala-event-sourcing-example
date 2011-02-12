82f51f33191d47226af2b2d9f962ee7ae5f4a612
Erik Rozendaal <erik@deler.org>
2011-02-12 22:26:50 +0100

    Removed direct use of UUID.

--- src/main/scala/eventstore/EventStore.scala
+++ src/main/scala/eventstore/EventStore.scala
@@ -1,10 +1,8 @@
 package com.zilverline.es2
 
-import java.util.UUID
 import scala.collection._
 
 package eventstore {
-
   import eventing._
 
   class EventStore {
@@ -15,13 +13,13 @@ package eventstore {
       listeners foreach {callback => callback(event.source, event)}
     }
 
-    def load(source: UUID): Iterable[Event] = storedEvents.getOrElse(source, Iterable.empty)
+    def load(source: EventSourceIdentifier): Iterable[Event] = storedEvents.getOrElse(source, Iterable.empty)
 
     def addListener(callback: EventListener) {
       listeners += callback;
     }
 
-    private val storedEvents: mutable.Map[UUID, mutable.Queue[Event]] = mutable.Map()
+    private val storedEvents: mutable.Map[EventSourceIdentifier, mutable.Queue[Event]] = mutable.Map()
     private val listeners: mutable.Queue[EventListener] = mutable.Queue()
   }
 
