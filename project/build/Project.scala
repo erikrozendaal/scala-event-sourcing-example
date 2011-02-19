6b2e5f9f9828df387e3a3ce4da3c34bdc77d0dee
Erik Rozendaal <erik@deler.org>
2011-02-19 11:54:49 +0100

    Added unchecked option to scalac.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,6 +1,8 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) with growl.GrowlingTests {
+  override def compileOptions = super.compileOptions ++ Seq(Unchecked)
+
   val squeryl = "org.squeryl" % "squeryl_2.8.1" % "0.9.4-RC3" withSources()
   val scalaz = "com.googlecode.scalaz" % "scalaz-core_2.8.0" % "5.0" withSources()
   val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.7" % "test" withSources()
