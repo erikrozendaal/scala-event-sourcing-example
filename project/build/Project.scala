112bd4da6f5da16c70cf286f6d0e2cacb106fc46
Erik Rozendaal <erik@deler.org>
2011-02-20 12:33:40 +0100

    Added IDEA plugin to project.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,8 +5,10 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with growl.Growlin
 
   val squeryl = "org.squeryl" % "squeryl_2.8.1" % "0.9.4-RC3" withSources()
   val scalaz = "com.googlecode.scalaz" % "scalaz-core_2.8.0" % "5.0" withSources()
+  val liftweb_json = "net.liftweb" % "lift-json_2.8.1" % "2.2" withSources()
+
+  val junit = "junit" % "junit" % "4.8.2" % "test" withSources()
   val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.7" % "test" withSources()
   val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8" % "test" withSources()
   val h2 = "com.h2database" % "h2" % "1.3.151" % "test" withSources()
-  val liftweb_json = "net.liftweb" % "lift-json_2.8.1" % "2.2" withSources()
 }
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,5 +1,9 @@
 import sbt._
+
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val lessRepo = "lessis repo" at "http://repo.lessis.me"
   val growl = "me.lessis" % "sbt-growl-plugin" % "0.0.5"
+
+  val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+  val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.3.0"
 }
